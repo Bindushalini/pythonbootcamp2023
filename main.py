c14d93dd8e94fc22292e581d6fe3e4bdfac71a41
Bindushalini <bindu239681@gmail.com>
2023-09-09 13:53:26 +0530

    Day 1

---
+++ main.py
@@ -0,0 +1,12 @@
+# basics of python
+print('Hello world!!')
+print("Hello world!\nHello world!\nYikes")
+print('Hello Bindu shalini ' + ' ' + 'how is ur day?')
+input("Would you like some tea?")
+x = 12
+y = 13
+tmp = x
+x = y
+y = tmp
+
+print(x, y)
