63d6e903a8bf81c3b46552e4558d08cbf6addb52
U-GLOBAL\binduhc <binduhc@PC-BINDUHC.global.cadence.com>
2023-09-23 22:17:31 +0530

    DAY 2

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+# Day 1
 # basics of python
 print('Hello world!!')
 print("Hello world!\nHello world!\nYikes")
 print('Hello Bindu shalini ' + ' ' + 'how is ur day?')
-input("Would you like some tea?")
+# input("Would you like some tea?")
 x = 12
 y = 13
 tmp = x
@@ -10,3 +11,25 @@ x = y
 y = tmp
 
 print(x, y)
+x = "FPGA_0A"
+y = x.lstrip("FPGA_")
+print(y)
+
+# Day 2:
+print("hello"[4])
+
+print(123+345)
+print(123_56)
+
+# float
+print(3.1416)
+#boolen -true /false
+
+x = len(input("name please?"))
+print(type(x))
+print(" your name has " + str(x) + " chars")
+
+score = 1
+score1 = 1.0
+print(f"score is {score}, {score1}")
+
