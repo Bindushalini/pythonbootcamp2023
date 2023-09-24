d3b504ada1e2ce06cf83923f1acdf94b67cc3bd5
U-GLOBAL\binduhc <binduhc@PC-BINDUHC.global.cadence.com>
2023-09-24 20:12:02 +0530

    DAY 4

---
+++ main2.py
@@ -0,0 +1,14 @@
+#Day 4
+# import random
+#
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+#
+# random_float = random.random()# generates between 0 and 1
+# print(random_float)
+
+# lists
+dirty_dozen = ["strawberries", "Necta" ]
+vege = ["potato", "tomato"]
+two_list = [dirty_dozen, vege]
+print(two_list)
