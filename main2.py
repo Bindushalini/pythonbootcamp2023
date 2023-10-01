aa0a1492a0610705e5d3b8fcab1af2d62a483f4f
Bindushalini <bindu239681@gmail.com>
2023-10-01 12:22:57 +0530

    DAY 9

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
-#Day 4
+# Day 4
 # import random
 #
 # random_integer = random.randint(1, 10)
@@ -23,18 +23,73 @@
 #     print("world")
 # abc()
 # day 8:
-def greet(name):
-    print("I am inside greet function, hi "+name)
-    print("Second line")
-    print("ending")
+# def greet(name):
+#     print("I am inside greet function, hi "+name)
+#     print("Second line")
+#     print("ending")
+#
+#
+# greet("poser")
+#
+#
+# def greet(name, location):
+#     print(f"inside new function of greet with arguments: name {name}, at location {location}")
+#
+#
+# greet(name="Bindu", location="Bangalore")
+
+# Day 9:
+# dictionaries
+my_dict = {0: "Monday", 1: "Tuesday", 2: "Wednesday"}
+print(my_dict[1])
+my_dict[5] = "Friday"
+print(my_dict)
+
+for x in my_dict:
+    print(my_dict[x])
+
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+student_grades = {}
 
 
-greet("poser")
+def get_grades(score):
+    grade = ""
+    if 91 <= score <= 100:
+        grade = "Outstanding"
+    elif 81 <= score <= 90:
+        grade = "Exceeds Expectations"
+    elif 71 <= score <= 80:
+        grade = "Acceptable"
+    elif score <= 70:
+        grade = "Fail"
+    else:
+        pass
+    return grade
 
 
-def greet(name, location):
-    print(f"inside new function of greet with arguments: name {name}, at location {location}")
+for value in student_scores:
+    student_grades[value] = get_grades(student_scores[value])
 
+print(student_grades)
 
-greet(name="Bindu", location="Bangalore")
+state = {
+    "Karnataka": ["rice", "Bangalore", "Kannada"],
+    "Maharashtra": ["Jowar", "Mumbai", "Marathi"],
+    "Kerala": {"Main_crops": ["Coconuts", "Tea", "Cloves"], "captial": "Thiruvananthapuram",
+               "local_language": "Malayalam"}
+}
+print(state)
+print(state["Kerala"]["Main_crops"])
 
+Education = [
+    {"Baby": "basic of languages", "age_group": "1-5"},
+    {"Preschooler": "Letters, numbers, games", "age_group": "5-7"}
+]
+print(Education[1]["age_group"])
