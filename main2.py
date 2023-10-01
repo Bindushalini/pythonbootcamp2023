351775fa1ce0f4c0b9dd302f06f2b526df584b91
Bindushalini <bindu239681@gmail.com>
2023-10-01 18:21:46 +0530

    DAY 10

--- main2.py
+++ main2.py
@@ -40,56 +40,71 @@
 
 # Day 9:
 # dictionaries
-my_dict = {0: "Monday", 1: "Tuesday", 2: "Wednesday"}
-print(my_dict[1])
-my_dict[5] = "Friday"
-print(my_dict)
+# my_dict = {0: "Monday", 1: "Tuesday", 2: "Wednesday"}
+# print(my_dict[1])
+# my_dict[5] = "Friday"
+# print(my_dict)
+#
+# for x in my_dict:
+#     print(my_dict[x])
+#
+# student_scores = {
+#     "Harry": 81,
+#     "Ron": 78,
+#     "Hermione": 99,
+#     "Draco": 74,
+#     "Neville": 62,
+# }
+#
+# student_grades = {}
+#
+#
+# def get_grades(score):
+#     grade = ""
+#     if 91 <= score <= 100:
+#         grade = "Outstanding"
+#     elif 81 <= score <= 90:
+#         grade = "Exceeds Expectations"
+#     elif 71 <= score <= 80:
+#         grade = "Acceptable"
+#     elif score <= 70:
+#         grade = "Fail"
+#     else:
+#         pass
+#     return grade
+#
+#
+# for value in student_scores:
+#     student_grades[value] = get_grades(student_scores[value])
+#
+# print(student_grades)
+#
+# state = {
+#     "Karnataka": ["rice", "Bangalore", "Kannada"],
+#     "Maharashtra": ["Jowar", "Mumbai", "Marathi"],
+#     "Kerala": {"Main_crops": ["Coconuts", "Tea", "Cloves"], "captial": "Thiruvananthapuram",
+#                "local_language": "Malayalam"}
+# }
+# print(state)
+# print(state["Kerala"]["Main_crops"])
+#
+# Education = [
+#     {"Baby": "basic of languages", "age_group": "1-5"},
+#     {"Preschooler": "Letters, numbers, games", "age_group": "5-7"}
+# ]
+# print(Education[1]["age_group"])
 
-for x in my_dict:
-    print(my_dict[x])
+# Day 10:
 
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62,
-}
-
-student_grades = {}
+def format_name(first_name, last_name):
+    if first_name == "" or last_name == "":
+        return "Emptystring", "EmptyString"
+    first_name = first_name.title()
+    last_name = last_name.title()
+    return first_name, last_name
 
 
-def get_grades(score):
-    grade = ""
-    if 91 <= score <= 100:
-        grade = "Outstanding"
-    elif 81 <= score <= 90:
-        grade = "Exceeds Expectations"
-    elif 71 <= score <= 80:
-        grade = "Acceptable"
-    elif score <= 70:
-        grade = "Fail"
-    else:
-        pass
-    return grade
+update_f_name, update_l_name = format_name("bindu", "viGnesh")
+update_f_name, update_l_name = format_name("", "x")
 
-
-for value in student_scores:
-    student_grades[value] = get_grades(student_scores[value])
-
-print(student_grades)
-
-state = {
-    "Karnataka": ["rice", "Bangalore", "Kannada"],
-    "Maharashtra": ["Jowar", "Mumbai", "Marathi"],
-    "Kerala": {"Main_crops": ["Coconuts", "Tea", "Cloves"], "captial": "Thiruvananthapuram",
-               "local_language": "Malayalam"}
-}
-print(state)
-print(state["Kerala"]["Main_crops"])
-
-Education = [
-    {"Baby": "basic of languages", "age_group": "1-5"},
-    {"Preschooler": "Letters, numbers, games", "age_group": "5-7"}
-]
-print(Education[1]["age_group"])
+print(update_f_name, update_l_name)
