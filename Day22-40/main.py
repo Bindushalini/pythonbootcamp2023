441a7afb7bf0c6c22e1bfc6a4879893ec46fc252
Bindushalini <bindu239681@gmail.com>
2023-10-19 16:28:46 +0530

    Day 23

--- Day22-40/main.py
+++ Day22-40/main.py
@@ -7,4 +7,11 @@
 # 4. bounce back
 # 5. detect collision
 # 6. update score, reduce / increase the score
+#
+#
+#  Day 23: Turtle capstone project
 
+# 1. create a turtle and navigate it using up keypress.
+# 2. create a car manager to create cars at required positon, size, and move
+# 3. detect collision and level up conditions
+# 4. display the score / game over sequence
