c42f361d1fe0cd57f1f13de898c980ec641b86e0
Bindushalini <bindu239681@gmail.com>
2023-11-22 20:54:14 +0530

    Day 35

--- Day22-40/main.py
+++ Day22-40/main.py
@@ -184,16 +184,19 @@ import datetime
 # data = resp.json()['iss_position']['latitude']
 # # data = resp.json()
 # print(data)
-params = {
-    "lat": 12.971599,
-    "lng": 77.594566
-}
-resp1 = requests.get(url="https://api.sunrise-sunset.org/json", params=params)
-resp1.raise_for_status()
-data = resp1.json()
-print(data)
-sunr = int(data["results"]["sunrise"].split(":")[0])
-suns = int(data["results"]["sunset"].split(":")[0])
-
-now = datetime.datetime.now()
-print(now.hour)
+# params = {
+#     "lat": 12.971599,
+#     "lng": 77.594566
+# }
+# resp1 = requests.get(url="https://api.sunrise-sunset.org/json", params=params)
+# resp1.raise_for_status()
+# data = resp1.json()
+# print(data)
+# sunr = int(data["results"]["sunrise"].split(":")[0])
+# suns = int(data["results"]["sunset"].split(":")[0])
+#
+# now = datetime.datetime.now()
+# print(now.hour)
+#
+# day 35
+# twilio API is used to send text messages
---
+++ Day22-40/rainChecker/main.py
@@ -0,0 +1,24 @@
+
+import requests
+api_key = "af58968c3f92734b128348a17517d3e6"
+parameters = {
+    "lat": 12.971599,
+    "lon": 77.594566,
+    "appid": api_key,
+    "exclude": "current,minutely,daily,alerts"
+}
+data_response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+data_response.raise_for_status()
+weather_data_hourly_list = data_response.json()['hourly']
+is_rainy_day = False
+for _ in weather_data_hourly_list[:12]:
+    hourly_data = _
+    weather_id = hourly_data['weather'][0]['id']
+    print(weather_id)
+    if int(weather_id) < 700:
+        # print(f"bring an umbrella, weather is {hourly_data['weather'][0]['description']}")
+        is_rainy_day = True
+
+if is_rainy_day:
+#     send message using twilio
+
